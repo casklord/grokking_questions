30e52ceaa1c86160810f5781feb7c10c7e21e917
Isaac Joshi <isaacjoshi@gmail.com>
2022-07-19 23:25:40 +1000

    more merge intervals questions

--- Problems/merge_intervals.java
+++ Problems/merge_intervals.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 class Interval {
   int start;
   int end;
@@ -99,30 +100,38 @@ class MergeIntervals {
   }
 }
 
-import java.util.*;
-
-class Interval {
-  int start;
-  int end;
-
-  public Interval(int start, int end) {
-    this.start = start;
-    this.end = end;
-  }
-};
-
 class InsertInterval {
   public static List<Interval> insert(List<Interval> intervals, Interval newInterval) {
 
+    if (intervals == null || intervals.size() == 0)
+      return Arrays.asList(newInterval);
+
     int start = newInterval.start;
     int end = newInterval.end;
+    List<Interval> newIntervals = new ArrayList<>();
+    Boolean inserted = false;
     
     for (Interval i : intervals){
-      if (i.end < newInterval.start)
-        continue;
-      
-      
+      // if the current interval is after newInterval and doesn't overlap then insert
+      if (!inserted && i.start > end){
+        newIntervals.add(new Interval(start, end));
+        inserted = true;
+      }
+
+      // if the current interval does not overlap with newInterval
+      if (i.end < start || i.start > end ){
+        newIntervals.add(i);
+      } else if (i.start < end) {
+        start = Math.min(start, i.start);
+        end = Math.max(end, i.end);
+      }      
     }
+
+    // if new interval would be the last inserted
+    if (!inserted)
+      newIntervals.add(new Interval(start, end));
+    
+    return newIntervals;
   }
 
   public static List<Interval> insertSolution(List<Interval> intervals, Interval newInterval) {
@@ -179,6 +188,77 @@ class InsertInterval {
     for (Interval interval : InsertInterval.insert(input, new Interval(1, 4)))
       System.out.print("[" + interval.start + "," + interval.end + "] ");
     System.out.println();
+  }
+}
+
+class IntervalsIntersection {
+  public static Interval[] merge(Interval[] arr1, Interval[] arr2) {
+
+    List<Interval> intersection = new ArrayList<>();
+    int i = 0; int j = 0;
+
+    while(i < arr1.length && j < arr2.length){
+      Interval i1 = arr1[i];
+      Interval i2 = arr2[j];
+      
+      if (
+        (i1.start >= i2.start && i1.start <= i2.end) || // i1 in i2
+        (i2.start >= i1.start && i2.start <= i1.end)    // i2 in i1
+          ) {
+        intersection.add(new Interval(Math.max(i1.start, i2.start), Math.min(i1.end, i2.end)));
+      }
+
+      if (i1.end < i2.end){
+        i ++;
+      } else {
+        j ++;
+      }
+      
+    }
+
+    return intersection.toArray(new Interval[intersection.size()]);
+
+  }
+  public static Interval[] mergeSolution(Interval[] arr1, Interval[] arr2) {
+    List<Interval> result = new ArrayList<Interval>();
+    int i = 0, j = 0;
+    while (i < arr1.length && j < arr2.length) {
+      // check if the interval arr[i] intersects with arr2[j]
+      // check if one of the interval's start time lies within the other interval
+      if ((arr1[i].start >= arr2[j].start && arr1[i].start <= arr2[j].end)
+          || (arr2[j].start >= arr1[i].start && arr2[j].start <= arr1[i].end)) {
+        // store the intersection part
+        result.add(new Interval(Math.max(arr1[i].start, arr2[j].start), 
+                                Math.min(arr1[i].end, arr2[j].end)));
+      }
+
+      // move next from the interval which is finishing first
+      if (arr1[i].end < arr2[j].end)
+        i++;
+      else
+        j++;
+    }
+
+    return result.toArray(new Interval[result.size()]);
+  }
+
+  public static void main(String[] args) {
+    Interval[] input1 = new Interval[] { new Interval(1, 3), new Interval(5, 6), 
+                                         new Interval(7, 9) };
+    Interval[] input2 = new Interval[] { new Interval(2, 3), new Interval(5, 7) };
+    Interval[] result = IntervalsIntersection.merge(input1, input2);
+    System.out.print("Intervals Intersection: ");
+    for (Interval interval : result)
+      System.out.print("[" + interval.start + "," + interval.end + "] ");
+    System.out.println();
+
+    input1 = new Interval[] { new Interval(1, 3), new Interval(5, 7), 
+                              new Interval(9, 12) };
+    input2 = new Interval[] { new Interval(5, 10) };
+    result = IntervalsIntersection.merge(input1, input2);
+    System.out.print("Intervals Intersection: ");
+    for (Interval interval : result)
+      System.out.print("[" + interval.start + "," + interval.end + "] ");
   }
 }
 
